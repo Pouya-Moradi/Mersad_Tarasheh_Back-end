8f8bf217d1deb76f690876ad19af5fa35b5d6e40
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-04 12:50:28 +0330

    Add relation between Product and Comment

--- review/models.py
+++ review/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from store.models import Product
 
 
 class Comment(models.Model):
 
     content = models.CharField(max_length=1023, verbose_name="متن نظر")
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', verbose_name="کالا")
 
     is_approved = models.BooleanField(default=False, null=False, blank=False, verbose_name="وضعیت تایید نظر")
 
