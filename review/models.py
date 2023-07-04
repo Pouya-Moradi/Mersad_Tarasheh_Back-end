674d02ef818d189729b31688d01c302172067208
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-04 13:00:46 +0330

    Add Rating model

--- review/models.py
+++ review/models.py
@@ -8,3 +8,8 @@ class Comment(models.Model):
     is_approved = models.BooleanField(default=False, null=False, blank=False, verbose_name="وضعیت تایید نظر")
 
     created_at = models.CharField(max_length=31, null=True, blank=True, verbose_name="زمان ثبت")
+
+
+class Rating(models.Model):
+
+    score = models.PositiveSmallIntegerField(choices=range(1, 6), default=1, verbose_name="امتیاز")
