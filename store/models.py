2e7edcecbe68823f6e2100e2a151899695d81e25
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-04 12:20:25 +0330

    Add Collection model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Collection(models.Model):
+
+    title = models.CharField(max_length=32, verbose_name="عنوان دسته بندی")
+
+    created_at = models.CharField(max_length=31, null=True, blank=True, verbose_name="زمان ثبت")
+    updated_at = models.CharField(max_length=31, null=True, blank=True, verbose_name="زمان به روزرسانی")
+
+    class Meta:
+        verbose_name_plural = "دسته بندی ها"
+        verbose_name = "دسته بندی"
