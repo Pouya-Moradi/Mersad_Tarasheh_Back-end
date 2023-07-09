9e1c5fff2c7dbf162a4accc48dd86e8113f6975d
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-09 14:26:32 +0330

    Add OrderItem view

--- order/views.py
+++ order/views.py
@@ -6,3 +6,8 @@ from .serializers import OrderSerializer, OrderItemSerializer
 class OrderViewSet(ModelViewSet):
     queryset = Order
     serializer_class = OrderSerializer
+
+
+class OrderItemViewSet(ModelViewSet):
+    queryset = OrderItem
+    serializer_class = OrderItemSerializer
