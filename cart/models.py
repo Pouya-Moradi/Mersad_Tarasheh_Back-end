d075ca54f55a4ee06cfbc62bc9f4b4fa7eeac627
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-09 09:55:15 +0330

    Add product field to CartItem model

--- cart/models.py
+++ cart/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from store.models import Product
 
 
 class Cart(models.Model):
@@ -9,4 +10,8 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart', 'product']]
