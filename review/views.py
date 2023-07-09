b7c69e7e998006077b551527174bfb2714a6729b
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-09 14:23:41 +0330

    Add Rating view

--- review/views.py
+++ review/views.py
@@ -6,3 +6,8 @@ from .serializers import CommentSerializer, RatingSerializer
 class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+
+class RatingViewSet(ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
