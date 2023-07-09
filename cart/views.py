cab2dce919c614e7f58218978120c4f6432f0542
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-09 14:10:51 +0330

    Add Retrieve and Destroy to Cart view

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,9 @@
-from rest_framework.mixins import CreateModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet
 from .models import Cart
 from .serializers import CartSerializer
 
 
-class CartViewSet(CreateModelMixin, GenericViewSet):
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
