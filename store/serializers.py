badf02880b43553bead937d5212cb9f1c1d98052
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-09 09:29:27 +0330

    Add ProductSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from .models import Collection
+from .models import Collection, Product
 
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
         fields = ['id', 'title', 'created_at', 'updated_at']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'unit_price', 'discount_percentage', 'is_available', 'collection',
+                  'created_at', 'updated_at']
