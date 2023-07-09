4d4f72ba05053f5f9681c22712e6a719d4e3514c
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-09 13:48:06 +0330

    Add order_items field to OrderSerializer

--- order/serializers.py
+++ order/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from .models import Order, OrderItem
 
 
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ['id', 'payment_status', 'created_at']
-
-
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = ['id', 'product', 'unit_price', 'quantity']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'payment_status', 'order_items', 'created_at']
