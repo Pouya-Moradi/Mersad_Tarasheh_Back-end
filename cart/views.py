2271e7e3b36dae4db7152c247c7c08ebe51fd5ba
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-09 14:03:33 +0330

    Add Cart view

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.viewsets import GenericViewSet
+from .models import Cart
+from .serializers import CartSerializer
 
-# Create your views here.
+
+class CartViewSet(CreateModelMixin, GenericViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
