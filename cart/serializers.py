2cb1774592bfc2c9e510cf241672adec9e55291e
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-09 14:06:22 +0330

    Add cart_items to CartSerializer

--- cart/serializers.py
+++ cart/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from .models import Cart, CartItem
 
 
-class CartSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cart
-        fields = ['id', 'created_at']
-
-
 class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = ['id', 'product', 'quantity']
+
+
+class CartSerializer(serializers.ModelSerializer):
+    cart_items = CartItemSerializer(many=True)
+
+    class Meta:
+        model = Cart
+        fields = ['id','cart_items', 'created_at']
