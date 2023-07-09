da81bd4c11ed6fcbca9f2fd4aaef4c15399a9cc1
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-09 09:48:16 +0330

    Add Cart model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="زمان ثبت")
