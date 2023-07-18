30adff3b07af2bea77e3a8087c18dca63a391c3c
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-18 09:44:46 +0330

    Add auth app to INSTALLED_APPS and set AUTH_USER_MODEL in the settings module

--- Mersad_Tarasheh_Backend/settings.py
+++ Mersad_Tarasheh_Backend/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'cart',
     'order',
     'review',
-    'store'
+    'store',
+    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authentication.User'
