aa6890c4852b62819c85f9b5857428eac229aed8
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-18 12:19:20 +0330

    Set apps' urls references in the main urls module

--- Mersad_Tarasheh_Backend/urls.py
+++ Mersad_Tarasheh_Backend/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authentication/', include('authentication.urls')),
+    path('cart/', include('cart.urls')),
+    path('order/', include('order.urls')),
+    path('review/', include('review.urls')),
+    path('store/', include('store.urls')),
 ]
 
 if settings.DEBUG:
