8013f84c96f2389825e6f0a77424c1a49f2d35cb
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-18 11:53:10 +0330

    Add Customer Model

--- authentication/models.py
+++ authentication/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from utilities.constants import STATE_CHOICES
 
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(unique=True, max_length=11)
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    display_name = models.CharField(max_length=64, null=False, blank=False)
+
+    state = models.PositiveIntegerField(choices=STATE_CHOICES)
+    city = models.CharField(max_length=32, null=False, blank=False)
+    address = models.CharField(max_length=256, null=False, blank=False)
+    zip_code = models.CharField(max_length=10, null=False, blank=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
