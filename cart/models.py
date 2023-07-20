7f94f8c7f3fb65d0ee517e373d6a169f3836d638
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-20 11:58:28 +0330

    Revising Cart and CartItem APIs

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
 from store.models import Product
@@ -11,7 +12,7 @@ class Cart(models.Model):
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items', verbose_name='سبد خرید')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='محصول')
-    quantity = models.PositiveSmallIntegerField(verbose_name='تعداد')
+    quantity = models.PositiveSmallIntegerField(validators=[MinValueValidator(1)], verbose_name='تعداد')
 
     class Meta:
         unique_together = [['cart', 'product']]
--- cart/views.py
+++ cart/views.py
@@ -1,16 +1,26 @@
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from .models import Cart, CartItem
-from .serializers import CartSerializer, CartItemSerializer
+from .serializers import CartSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer
 
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
-    queryset = Cart.objects.all()
+    queryset = Cart.objects.prefetch_related('cart_items__product').all()
     serializer_class = CartSerializer
 
 
 class CartItemViewSet(ModelViewSet):
-    serializer_class = CartItemSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
 
     def get_queryset(self):
-        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk']).select_related('product')
