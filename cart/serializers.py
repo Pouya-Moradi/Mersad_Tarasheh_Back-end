afce7b9bd6f7690a39fd2c864f2ba949575624f5
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-20 11:06:43 +0330

    Revising Cart and CartItem serializers

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,15 +1,36 @@
 from rest_framework import serializers
 from .models import Cart, CartItem
+from store.models import Product
+
+
+class CartItemProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price']
 
 
 class CartItemSerializer(serializers.ModelSerializer):
+    product = CartItemProductSerializer()
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, cart_item:CartItem):
+        return cart_item.quantity * cart_item.product.unit_price
+
     class Meta:
-        model = ['id', 'product', 'quantity']
+        model = CartItem
+        fields = ['id', 'product', 'quantity']
 
 
 class CartSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField()
     cart_items = CartItemSerializer(many=True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, cart):
+        sum([cart_item.quantity * cart_item.product.unit_price for cart_item in cart.cart_items.all()])
 
     class Meta:
         model = Cart
-        fields = ['id','cart_items', 'created_at']
+        fields = ['id', 'cart_items']
+
+        read_only_fields = ['id']
