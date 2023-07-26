99a90c6578fcc85d95bf4548ba50de8a19a0738a
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-26 10:19:14 +0330

    Upgrading the Collection Admin

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+
 from . import models
 
 
@@ -13,6 +17,19 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created_at', 'updated_at']
-    list_editable = []
+    list_display = ['title', 'products_count', 'created_at', 'updated_at']
     list_per_page = 10
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = reverse('admin:store_product_changelist') + '?' + urlencode(
+            {
+                'collection__id': str(collection.id)
+            }
+        )
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count = Count('products')
+        )
