e78b0b2371282cdcc97a367f3f8f61439aea5101
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-28 23:22:09 +0330

    Updating the requirements

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -10,6 +10,7 @@ class UserCreateSerializer(BaseUserCreateSerializer):
 
 class CustomerSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField()
+
     class Meta:
         model = Customer
         fields = ['id', 'user_id', 'display_name', 'state', 'city', 'address', 'zip_code', 'created_at', 'updated_at']
