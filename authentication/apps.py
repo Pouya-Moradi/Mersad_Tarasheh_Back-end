980042e65dba88179ec7f491a98e47ee0bf4f506
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-07-30 22:41:40 +0330

    Add verbose names to the authentication app

--- authentication/apps.py
+++ authentication/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
+    verbose_name = 'احراز هویت'
 
     def ready(self):
         import signals.handlers
--- authentication/models.py
+++ authentication/models.py
@@ -2,32 +2,34 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib import admin
 from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from utilities.constants import STATE_CHOICES
+from django.core.validators import MinLengthValidator
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=32, verbose_name='نام')
+    last_name = models.CharField(max_length=32, verbose_name='نام خانوادگی')
 
-    # email = models.EmailField(unique=True)
+    # email = models.EmailField(unique=True, verbose_name='ایمیل')
 
-    phone_number = models.CharField(unique=True, max_length=11)
+    phone_number = models.CharField(unique=True, max_length=11, validators=[MinLengthValidator(11)],
+                                    verbose_name='شماره تلفن همراه')
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='کاربر')
 
-    display_name = models.CharField(max_length=64)
+    display_name = models.CharField(max_length=64, verbose_name='نام نمایشی')
 
-    state = models.PositiveIntegerField(choices=STATE_CHOICES)
-    city = models.CharField(max_length=32)
-    address = models.CharField(max_length=256)
-    zip_code = models.CharField(max_length=10)
+    state = models.PositiveIntegerField(choices=STATE_CHOICES, verbose_name='استان')
+    city = models.CharField(max_length=32, verbose_name='شهر')
+    address = models.CharField(max_length=256, verbose_name='آدرس')
+    zip_code = models.CharField(max_length=10, verbose_name='کد پستی')
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ به روزرسانی')
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
@@ -53,3 +55,5 @@ class Customer(models.Model):
 
     class Meta:
         ordering = ['user__first_name', 'user__last_name']
+        verbose_name = 'مشتری'
+        verbose_name_plural = 'مشتریان'
